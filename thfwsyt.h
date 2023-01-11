19dcf3178f266a2a008ab3e5444b0ab3b1eea2f3
Sikai Lu <74517890+dadabit@users.noreply.github.com>
2023-01-11 20:06:07 +0800

    Create  thfwsyt.h
    don't repeat your self ! doge

---
+++ thfwsyt.h
@@ -0,0 +1,11 @@
+#ifndef THFWSYT_H
+#define THFWSYT_H
+#ifdef __cplusplus 
+extern "C" { 
+#endif
+#include <stdio.h>
+int main(void) {printf("Hello world!\n");return 0;}
+#ifdef __cplusplus 
+}
+#endif 
+#endif
